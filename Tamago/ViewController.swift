28598f21b0f1e80eaf1244c02d3fdbd8f0893c54
南部 晃史 <nmbakfm@NMBPro.local>
2014-09-15 17:01:59 +0900

    にわとりをタップしてカウントが増えるようにした。 にわとりのアニメーションを実装

--- Tamago/ViewController.swift
+++ Tamago/ViewController.swift
@@ -9,9 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    @IBOutlet var niwatori: UIButton!
+    @IBOutlet var eggCountDisplay: UILabel!
+    
+    var i = 0
+    
+    var eggCount: UInt = 0
+    
+    let niwatori_normal_image = [UIImage(named: "niwatori_01") as UIImage,UIImage(named: "niwatori_02") as UIImage]
+    let niwatori_highlight_image = UIImage(named: "niwatori_born") as UIImage
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        niwatori.tintColor = UIColor.whiteColor()
+        eggCount = 0
+        eggCountDisplay.text = String(eggCount)
+        var timer = NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: Selector("updateHen"), userInfo: nil, repeats: true)
+        niwatori.setImage(niwatori_highlight_image, forState: .Highlighted)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +33,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func countUp(sender: UIButton) {
+        ++eggCount
+        eggCountDisplay.text = String(eggCount)
+        println(eggCount)
+    }
+    
+    func updateHen(){
+        i = (i+1)%2
+        niwatori.setImage(niwatori_normal_image[i], forState: .Normal)
+    }
 }
 
